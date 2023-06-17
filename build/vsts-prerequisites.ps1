0461a2521a472b67ca23a145bef31df4d85956f0
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2023-06-18 00:50:33 +0200

    strings test fixes

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,6 +1,13 @@
-﻿Write-Host "Installing Pester" -ForegroundColor Cyan
-Install-Module Pester -Force -SkipPublisherCheck
-Write-Host "Installing PSFramework" -ForegroundColor Cyan
-Install-Module PSFramework -Force -SkipPublisherCheck
-Write-Host "Installing PSScriptAnalyzer" -ForegroundColor Cyan
-Install-Module PSScriptAnalyzer -Force -SkipPublisherCheck
+﻿Install-Module Microsoft.PowerShell.PSResourceGet -Force
+
+$modules = @(
+	'Pester'
+	'PSScriptAnalyzer'
+	'PSFramework'
+	'PSModuleDevelopment'
+)
+
+foreach ($module in $Modules) {
+	Write-Host "Installing $module" -ForegroundColor Cyan
+	Install-PSResource $module
+}
